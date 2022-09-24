eef6c0bfcf4188d005a8397232fb9404c1238261
t-ito-12 <t-ito.12@aol.com>
2022-09-24 20:54:29 +0900

    分の時間計算の誤りを修正

--- WorkingHoursCalculator/ViewModels/CalculationWindowViewModel.cs
+++ WorkingHoursCalculator/ViewModels/CalculationWindowViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Prism.Commands;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace WorkingHoursCalculator.ViewModels
